ede744513a61e74f9eddcca268f69de0830fcef6
Dima Petrenko <saisdima@mail.ru>
2016-10-07 11:31:57 +0300

    Added syntaxisAnalisatorActivity and xml File. Now empty.

---
+++ app/src/main/java/com/example/sails/database/SyntaxisAnalisatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.sails.database;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SyntaxisAnalisatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_syntaxis_analisator);
+    }
+}
